0681648cec333bc02a03df3424268c26e50be87b
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-08-20 00:15:32 -0400

    update

--- app.py
+++ app.py
@@ -912,8 +912,8 @@ if mode == "Single crystal":
                     ref_s1 = -ref_s1
 
                 # Actual s1 positions
-                s1_from = offset + angle_from - Q_offset - ref_s1
-                s1_to   = offset + angle_to - Q_offset - ref_s1
+                s1_from = offset + angle_from - Q_offset + ref_s1
+                s1_to   = offset + angle_to - Q_offset + ref_s1
 
                 # ====================================================
                 # kf side
